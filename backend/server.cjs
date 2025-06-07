06d8aaf8a7c966485be43fa85c06896aa86c3530
desivar <jillian_desire@yahoo.es>
2025-06-07 05:25:43 -0600

    changed server to use new package.json structure

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,4 +1,4 @@
-// backend/server.js
+// backend/server.cjs
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const mongoose = require='mongoose';
